de68a7835fd0ae07984922dbda56ec65fd7d81cd
deboranis <dc.nisen@hotmail.com>
2020-06-10 15:49:17 -0300

    quarta

---
+++ aula-03/loops-html/js/index.js
@@ -0,0 +1,29 @@
+// const soma = (x, y) => { return (x + y) }
+
+// soma(3, 4);
+
+/////////
+
+// const obj = {
+//     nome: "Mulheres, raça e classe",
+//     descrição: "Da mesma forma que gênero é a maneira como a raça é vivida."
+// }
+
+// const retornarBiografia = () => {
+//     console.log(obj.nome)
+//     console.log(obj.descrição)
+// }
+
+// retornarBiografia();
+
+// 3. Reescreva a função abaixo utilizando
+// Arrow Function 
+
+const objetoUsuario = (id, nome) => {
+    return {
+        id: id,
+        nome: nome
+    }
+}
+
+objetoUsuario(123, 'Debs')
